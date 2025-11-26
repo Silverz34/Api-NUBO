d840747acbe2a3b37675a2249d0db51bf425dc3b
Silverz34 <pradogordillomichael@gmail.com>
2025-11-25 20:56:47 -0600

    interfacesactivity

--- src/main/kotlin/domain/ports/ActivityRepository.kt
+++ src/main/kotlin/domain/ports/ActivityRepository.kt
@@ -1,4 +1,12 @@
 package domain.ports
 
-interface Activity {
+import domain.model.Activity
+
+
+interface ActivityRepository {
+    fun save(activity: Activity): Activity
+    fun findAll(): List<Activity>
+    fun findByTeacherId(teacherId: Int): List<Activity>
+    fun delete(activityId: Int)
+    fun findById(activityId: Int)
 }
