f8fddff40b0c656c864dc8123445c1dddc6ca9d2
Silverz34 <pradogordillomichael@gmail.com>
2025-11-25 23:15:39 -0600

    fun de hasheo

---
+++ src/main/kotlin/infrastructure/adapters/output/BCryptPassword.kt
@@ -0,0 +1,13 @@
+package infrastructure.adapters.output
+
+import domain.ports.PasswordEncoderPort
+import org.mindrot.jbcrypt.BCrypt
+
+class BCryptPassword : PasswordEncoderPort{
+    override fun encode(contrasena: String): String {
+        return BCrypt.hashpw(contrasena, BCrypt.gensalt())
+    }
+    override fun matches(contrasenaRaw: String, contrasenaHash: String): Boolean {
+        return BCrypt.checkpw(contrasenaRaw, contrasenaHash)
+    }
+}
