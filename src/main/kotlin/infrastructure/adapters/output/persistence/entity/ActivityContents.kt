2482ed6cedce215bc84b1a6f4af92f7f3df18c9f
Yael <243678@ids.upchiapas.edu.mx>
2025-11-30 19:45:54 -0600

    REVISIONES PARTE IV Cambios mínimos, los endpoints (o la mayoría de) de las actividades ya funcionan

--- src/main/kotlin/infrastructure/adapters/output/persistence/entity/ActivityContents.kt
+++ src/main/kotlin/infrastructure/adapters/output/persistence/entity/ActivityContents.kt
@@ -3,10 +3,10 @@ package infrastructure.adapters.output.persistence.entity
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.ReferenceOption
 
-object ActivityContents: UUIDTable("ContentActivity"){
-    val activityId = reference("activityId", activitiesTable, onDelete = ReferenceOption.CASCADE)
-    val texto = varchar("texto",100)
-    val imagenUrl = varchar("imagenUrl", 500)
-    val silabas = text("silabas").nullable()
-    val fonemas  = text("fonemas").nullable()
+object ActivityContents: UUIDTable("content"){
+    val activityId = reference("activity_id", activitiesTable, onDelete = ReferenceOption.CASCADE)
+    val texto = varchar("word",100)
+    val imagenUrl = varchar("image", 500)
+    val silabas = text("syllables").nullable()
+    val fonemas  = text("graphemes").nullable()
 }
